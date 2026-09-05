c08c4ff840ef2dfd94e4767cda9b6123d2920f8c
rakuda <abdulkaderbaghaffar@gmail.com>
2026-09-05 14:05:29 +0300

    add POST /material

--- src/main/java/com/example/costestimator/controller/MaterialController.java
+++ src/main/java/com/example/costestimator/controller/MaterialController.java
@@ -1,10 +1,11 @@
 package com.example.costestimator.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.costestimator.data.Material;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,5 +18,14 @@ public class MaterialController {
   @GetMapping("/materials") // when get request at materials return this
   public List<Material> listMaterials() {
     return materials;
+  }
+
+  @PostMapping("/materials")
+  public Material addMaterial(@RequestBody Material material) { // return type material from JSON made from material
+                                                                // object
+    material.setId(nextId);
+    nextId++;
+    materials.add(material);
+    return material;
   }
 }
